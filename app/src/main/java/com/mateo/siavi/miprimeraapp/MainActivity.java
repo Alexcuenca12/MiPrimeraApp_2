4943ee7acb5a4bb2da15a543b9af857534c095b2
Alexcuenca12 <91800258+Alexcuenca12@users.noreply.github.com>
2022-07-12 18:54:17 -0500

    Uso de Firebase

--- app/src/main/java/com/mateo/siavi/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/mateo/siavi/miprimeraapp/MainActivity.java
@@ -1,26 +1,87 @@
 package com.mateo.siavi.miprimeraapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnIngreso;
+    Button btnRegistro;
+    FirebaseAuth firebaseAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//Enlace con la vista
+        firebaseAuth=FirebaseAuth.getInstance();
         btnIngreso=findViewById(R.id.btnIngreso);//Enlace con el elemento
-        inicia_Control();
+        btnRegistro=findViewById(R.id.btnRegistro);//Enlace con el elemento
+        inicioControl();
     }
 
-    private void inicia_Control(){
+    private void inicioControl(){
+        btnRegistro.setOnClickListener(l-> registro_Usuario());
+        btnIngreso.setOnClickListener(l-> ingreso_Usuario());
+    }
+
+    private void registro_Usuario(){
+        EditText txt_Usuario = findViewById(R.id.txtUsuario);
+        EditText txtPass = findViewById(R.id.txtPassword);
+
+        if (!txt_Usuario.getText().toString().isEmpty()){
+            firebaseAuth.getInstance().createUserWithEmailAndPassword(txt_Usuario.getText().toString(),txtPass.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(),"Registro Correcto",Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(),"ERROR Registro",Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    }
+            );
+        }
+
+    }
+
+    private void ingreso_Usuario(){
+        EditText txt_Usuario =findViewById(R.id.txtUsuario);
+        EditText txtPass = findViewById(R.id.txtPassword);
+
+        if (!txt_Usuario.getText().toString().isEmpty()){
+            firebaseAuth.signInWithEmailAndPassword(txt_Usuario.getText().toString(),txtPass.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                cambiarPantalla();
+                                Toast.makeText(getApplicationContext(),"Ingreso Correcto",Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(),"ERROR Ingreso",Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    }
+                    );
+        }
+
+    }
+
+    private void cambiarPantalla(){
         btnIngreso.setOnClickListener(v -> {
             //Enlace con el txtUsuario
             EditText txtUsuario =findViewById(R.id.txtUsuario);
@@ -34,4 +95,5 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
 }
